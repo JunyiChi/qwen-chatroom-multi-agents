0b33c911a57f7e412e23cfff46af2d57a0cbea14
JunyiChi <24300120180@m.fudan.edu.cn>
2025-07-18 10:29:18 +0800

    清理项目：删除无关文件

--- my_charts_test.py
+++
@@ -1,6 +0,0 @@
-from pyecharts.charts import  Line
-line = Line()
-line.add_xaxis(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
-line.add_yaxis("Product A", [120, 132, 101, 134, 90, 230, 210])
-line.add_yaxis("Product B", [220, 182, 191, 234, 290, 330, 310])
-line.render()
